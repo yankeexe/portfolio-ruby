f0edcdea80bd4d025d2a598cc16a66fa6e6da9cc
Yankee <yankee.exe@gmail.com>
2017-01-27 12:43:09 +0545

    added image upload with paperclip

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -12,6 +12,25 @@ ActiveAdmin.register Post do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
+permit_params :title, :body, :image
 
+		show do |t|
+			attributes_table do
+				row :title
+				row :body
+				row :image do
+					post.image? ? image_tag(post.image.url, height: '100') : content_tag(:span, "No photo yet")
+				end
+			end
+		end
+
+		form :html => { :enctype => "multipart/form-data" } do |f|
+			f.inputs do
+				f.input :title
+				f.input :body
+				f.input :image, hint: f.post.image? ? image_tag(post.image.url, height: '100') : content_tag(:span, "Upload JPG/PNG/GIF image")
+			end
+			f.actions
+		end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170127055404) do
+ActiveRecord::Schema.define(version: 20170127063525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,8 +49,12 @@ ActiveRecord::Schema.define(version: 20170127055404) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
